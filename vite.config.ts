fc670192d5fa30cd7420d5f7894bdbab70ef1413
Kim Vigren <kim@jointacademy.com>
2025-10-03 22:57:53 +0200

    add: More aliases

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, './src'),
         '@assets': path.resolve(__dirname, './src/assets'),
+        '@components': path.resolve(__dirname, './src/components'),
       },
     },
   };
