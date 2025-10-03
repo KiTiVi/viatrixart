5e0f5976324c3da3be4a8586c8b7900f6e8a1885
Kim Vigren <kim@jointacademy.com>
2025-10-03 21:34:16 +0200

    fix: Base path issues

--- src/components/image.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  alt: string;
-  src: string;
-  className?: string;
-}
-
-export const Image: React.FC<Props> = ({ alt, src, className }) => (
-  <img src={`/viatrixart/${src}`} alt={alt} className={className} />
-);
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,5 +1,4 @@
 import { Link } from '@tanstack/react-router';
-import { Image } from './image';
 import { Nav } from './nav';
 import './sidebar.css';
 
@@ -7,16 +6,16 @@ export const Sidebar: React.FC = () => {
   return (
     <aside className="sidebar">
       <div className="sidebar-background-container">
-        <Image
+        <img
           alt="galaxy"
-          src="assets/ferenc-horvath-skcFiBu91AA-unsplash.jpg"
+          src="/assets/ferenc-horvath-skcFiBu91AA-unsplash.jpg"
           className="sidebar-background"
         />
       </div>
       <Link to="/">
-        <Image
+        <img
           alt="viatrixart.com"
-          src="assets/logo.png"
+          src="/assets/logo.png"
           className="sidebar-logo"
         />
       </Link>
