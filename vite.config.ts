caa8eefce8af1c19f00245c2d3333c5043b7a8ba
Kim Vigren <kim@jointacademy.com>
2025-10-03 22:30:37 +0200

    revert: Resolving paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
-import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,10 +12,4 @@ export default defineConfig({
     }),
     react(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-    },
-  },
 });
