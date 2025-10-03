56bde44a16425fd010967ed7b280975097c19b9a
Kim Vigren <kim@jointacademy.com>
2025-10-03 22:35:01 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/',
   plugins: [
     tanstackRouter({
       target: 'react',
